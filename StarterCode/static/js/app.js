301576bbd1fe25e867059c928cec4502f711ca00
jeaniewu <jeaniewu@jeaniewudeMBP.attlocal.net>
2022-06-21 20:34:40 -0700

    plot

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,144 +1,102 @@
-var option = "";
-var dataSet ;
-
-
-function init() {
-
-  d3.json("samples.json").then(function(data){
-    dataSet = data;
-
-    console.log(dataSet);
-    
-    displayMetaData(940,dataSet);
-    displayHBarChart(940,dataSet);
-    displayBubbleChart(940,dataSet);
-
-    var optionMenu = d3.select("#selDataset");
-
-    data.names.forEach(function(name){
-      optionMenu.append("option").text(name);
-    });
- })
-}
-
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
-
-function optionChanged(value) {
-    option = value;
-    displayMetaData(option,dataSet);
-    displayHBarChart(option,dataSet);
-    displayBubbleChart(option,dataSet);
-}
-
-function displayMetaData(option,dataSet) {
-    
-    
-    var mtdata = dataSet.metadata.filter(row => row.id == option);
-    d3.select("#sample-metadata").html(displayObject(mtdata[0]));
+function getPlots(id) {
+ 
+        d3.json("samples.json").then (sampledata =>{
+            console.log(sampledata)
+            var ids = sampledata.samples[0].otu_ids;
+            console.log(ids)
+            var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+            console.log(sampleValues)
+            var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+            console.log (labels)
         
-}
+            var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+            var OTU_id = OTU_top.map(d => "OTU " + d);
+            console.log(`OTU IDS: ${OTU_id}`)
+            var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+            console.log(`OTU_labels: ${labels}`)
+            var trace = {
+                x: sampleValues,
+                y: OTU_id,
+                text: labels,
+                marker: {
+                color: 'blue'},
+                type:"bar",
+                orientation: "h",
+            };
+  
+            var data = [trace];
+    
+            var layout = {
+                title: "Top 10 OTU",
+                yaxis:{
+                    tickmode:"linear",
+                },
+                margin: {
+                    l: 100,
+                    r: 100,
+                    t: 100,
+                    b: 30
+                }
+            };
+    
+        Plotly.newPlot("bar", data, layout);        
+            var trace1 = {
+                x: sampledata.samples[0].otu_ids,
+                y: sampledata.samples[0].sample_values,
+                mode: "markers",
+                marker: {
+                    size: sampledata.samples[0].sample_values,
+                    color: sampledata.samples[0].otu_ids
+                },
+                text:  sampledata.samples[0].otu_labels
+    
+            };
 
-function displayObject(obj) {
-    var str = "";
-    Object.entries(obj).forEach(([key,value]) => {
-        str += `<br>${key}:${value}</br>`;
-        if(key=="wfreq"){
-            buildGauge(value);
-            console.log("gauge value is:" +value);
-        }
+            var layout_2 = {
+                xaxis:{title: "OTU ID"},
+                height: 600,
+                width: 1000
+            };
+    
+            var data1 = [trace1];
+    
+        Plotly.newPlot("bubble", data1, layout_2); 
         
-    });
-    return str;
-}
-
-function displayHBarChart(option,dataSet) {
+        });
+    }  
+    function getDemoInfo(id) {
+        d3.json("samples.json").then((data)=> {
+            var metadata = data.metadata;
     
-    var barData = dataSet.samples.filter(sample => sample.id == option);
-    console.log(barData);
+            console.log(metadata)
+           var result = metadata.filter(meta => meta.id.toString() === id)[0];
+           var demographicInfo = d3.select("#sample-metadata");
+            
     
-
-    
-
-    var y = barData.map(row =>row.otu_ids);  
-    var y1 =[];
-
-    
-   
-    for(i=0;i<y[0].length;i++){
-        y1.push(`OTU ${y[0][i]}`);
+           demographicInfo.html("");
+            Object.entries(result).forEach((key) => {   
+                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+            });
+        });
     }
-
-    var x = barData.map(row =>(row.sample_values));
-    var text = barData.map(row =>row.otu_labels);
-    
-
-    var trace = {
-        x:x[0].slice(0,10),
-        y:y1.slice(0,10),
-        text:text[0].slice(0,10),
-        type:"bar",
-        orientation:"h",
-        
-    };
-
-    var data = [trace];
-
-    var layout = {
-        yaxis: {
-            autorange: "reversed" 
-        }
+    function optionChanged(id) {
+        getPlots(id);
+        getDemoInfo(id);
     }
-
     
-
+    function init() {
+        var dropdown = d3.select("#selDataset");
+ 
+        d3.json("samples.json").then((data)=> {
+            console.log(data)
     
-    Plotly.newPlot("bar",data,layout);
-}
-
-function displayBubbleChart(option,dataSet) {
-
-    var barData = dataSet.samples.filter(sample => sample.id == option);
-    console.log(barData); 
-
-    var x = barData.map(row =>row.otu_ids); 
-    var y = barData.map(row =>row.sample_values); 
-    var text = barData.map(row =>row.otu_labels);
-    var marker_size = barData.map(row =>row.sample_values);
-    var marker_color = barData.map(row =>row.otu_ids);
+            data.names.forEach(function(name) {
+                dropdown.append("option").text(name).property("value");
+            });
+            
+            getPlots(data.names[0]);
+            getDemoInfo(data.names[0]);
+        });
+    }
     
-    console.log(x[0]);
-    console.log(y[0]);
-    console.log(text);
-    
-    var trace1 = {
-        x:x[0],
-        y:y[0],
-        text: text[0],
-        mode:"markers",
-        marker: {
-            color: marker_color[0],
-            size: marker_size[0]
-        }
-        
-    };
-
-    var data = [trace1];
-
-    var layout = {
-        xaxis:{
-            title: "OTU ID"
-        }
-
-    };
-
-    Plotly.newPlot("bubble",data,layout);
-
-}
-
-
-
-init();
+    init();
